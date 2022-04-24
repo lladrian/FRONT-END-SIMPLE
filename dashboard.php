d3653fd05177444443c11199c9dd8d6c5ff37881
Adrian C. Manatad <89061206+lladrian@users.noreply.github.com>
2022-04-24 05:00:59 -0800

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,69 @@
+<div class="row">
+
+              <div class="flex-cointainer">
+                    <div class="flex-item">
+                      <span class="info-box-icon bg-navy elevation-1"><i class="fas fa-truck-loading"></i></span>
+                      <div class="info-box-content">
+                        <span class="info-box-text">Total Suppliers</span>
+                        <span class="info-box-number">
+                        
+                        </span>
+                      </div> <!-- /.info-box-content -->
+                    </div> <!-- /.info-box -->
+              </div>
+
+
+              <!-- /.col -->
+                <div class="flex-cointainer">
+                    <div class="flex-item">
+                        <span class="info-box-icon bg-success elevation-1"><i class="fas fa-boxes"></i></span>
+                        <div class="info-box-content">
+                          <span class="info-box-text">Total Items</span>
+                          <span class="info-box-number">
+                       
+                          </span>
+                        </div> 
+                      <!-- /.info-box-content -->
+                    </div> 
+                  <!-- /.info-box -->
+                </div>
+              <!-- /.col -->
+
+
+               <!-- /.col -->
+                 <div class="flex-cointainer">
+                    <div class="flex-item">
+                      <span class="info-box-icon bg-primary elevation-1"><i class="fas fa-file-invoice"></i></span>
+                      <!-- /.info-box-content -->
+                        <div class="info-box-content">
+                          <span class="info-box-text">Approve P.O.</span>
+                          <span class="info-box-number">
+                         
+                          </span>
+                        </div>
+                      <!-- /.info-box-content -->
+                    </div>
+                  <!-- /.info-box -->
+                </div>
+              <!-- /.col -->
+
+
+
+              <!-- /.col -->
+                <div class="flex-cointainer">
+                    <div class="flex-item">
+                      <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-file-invoice"></i></span>
+                       <!-- /.info-box-content -->
+                          <div class="info-box-content">
+                            <span class="info-box-text">Denied PO</span>
+                            <span class="info-box-number">
+                            
+                            </span>
+                          </div>
+                       <!-- /.info-box-content -->
+                    </div>
+                  <!-- /.info-box -->
+                </div>
+              <!-- /.col -->
+
+  </div>
